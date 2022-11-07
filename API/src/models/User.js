7e797e19e8ca423973804f10eed8e6b9c501f628
marco giabbani <mtgiabbani@gmail.com>
2022-11-07 18:53:34 -0300

    completes user model

---
+++ API/src/models/User.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+
+  sequelize.define('user', {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // here would be the idProf reference
+  },
+  {
+    timestamps: false,
+    createdAt: false
+});
+};
