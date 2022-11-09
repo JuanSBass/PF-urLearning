5411944cd739c0710dd048f4375afe7b3b95b0f0
JuanSBass <juan_santillan20@hotmail.com>
2022-11-09 17:06:26 -0600

    figma finish

--- Client/urLearning/src/components/Home/InfoService/InfoService.jsx
+++ Client/urLearning/src/components/Home/InfoService/InfoService.jsx
@@ -1,14 +1,33 @@
-import styles from "./InfoService.module.css"
-
+import styles from "./InfoService.module.css";
 
 export const InfoService = () => {
   return (
     <section className={styles.infocontainer}>
       <div className={styles.leftpurple}>
-        <h1>holi</h1>
+        <h1>¿Por qué elegirnos?</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          sunt distinctio maiores quasi qui necessitatibus nemo architecto est
+          eligendi? Veritatis tempore beatae, soluta assumenda obcaecati amet
+          magnam dolore error laborum. Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Eos exercitationem cupiditate, dicta quo repellendus
+          deleniti fuga, tempore perferendis facere architecto voluptatem
+          nesciunt, repudiandae quas molestiae nam sint? Quia, pariatur hic?
+        </p>
       </div>
       <div className={styles.rigthpurple}>
-        <h1>holix2</h1>
+        <div className={styles.points}>
+          <h1>1</h1>
+          <span>Una plataforma segura</span>
+        </div>
+        <div className={styles.points}>
+          <h1>2</h1>
+          <span>Promocionamos tu clase</span>
+        </div>
+        <div className={styles.points}>
+          <h1>3</h1>
+          <span>Estudia lo que te guste</span>
+        </div>
       </div>
     </section>
   );
