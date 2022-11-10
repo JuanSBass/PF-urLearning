1999b2b9ea410932e6032d91a987c8b37c218b23
JuanSBass <juan_santillan20@hotmail.com>
2022-11-09 22:06:53 -0600

    .env fixed!

--- Client/urLearning/src/main.jsx
+++ Client/urLearning/src/main.jsx
@@ -6,10 +6,8 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-axios.defaults.baseURL = "https://urlearning.onrender.com" || "http://localhost:3001";
+axios.defaults.baseURL =
+  import.meta.env.VITE_APP_API || "http://localhost:3001";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
