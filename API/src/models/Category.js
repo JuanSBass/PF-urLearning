ed2c0913947ddd14cd8bca300a065afa538cbb40
marco giabbani <mtgiabbani@gmail.com>
2022-11-10 08:33:44 -0300

    crates models

---
+++ API/src/models/Category.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "category",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+      createdAt: false,
+    }
+  );
+};
