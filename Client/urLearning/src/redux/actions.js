190a15442a0521e3ade91b987cd7570f44009da4
santiago restrepo <santiago.restrepo06@hotmal.com>
2022-11-10 14:17:35 -0500

    d

--- Client/urLearning/src/redux/actions.js
+++ Client/urLearning/src/redux/actions.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 export const GET_COURSES = "GET_COURSES";
 export const POST_COURSE = "POST_COURSE";
+export const GET_CHILD_CATEGORY = "GET_CHILD_CATEGORY";
+export const GET_CATEGORY = "GET_CATEGORY";
 export const POST_USER = "POST_USER";
 export const GET_DETAIL = "GET_DETAIL";
 
 export const getCourses = () => {
   try {
     return async function (dispatch) {
-      const response = await axios.get("/user");
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
       console.log(response);
       dispatch({ type: GET_COURSES, payload: response.data });
     };
@@ -20,6 +24,16 @@ export function postCourse(dataCourse) {
   return async function () {
     const json = await axios.post("/course", dataCourse);
     return json;
+  };
+}
+
+export function getChildCategory(idCategory) {
+  return async function (dispatch) {
+    const json = await axios.get("/childCategoriesFrom", idCategory);
+    return dispatch({
+      type: GET_CHILD_CATEGORY,
+      payload: json.data,
+    });
   };
 }
 
@@ -44,3 +58,13 @@ export const getDetail = (id) => {
     console.log(error.message);
   }
 };
+
+export function getCategory() {
+  return async function (dispatch) {
+    const json = await axios.get("/allCategories");
+    return dispatch({
+      type: GET_CATEGORY,
+      payload: json.data,
+    });
+  };
+}
