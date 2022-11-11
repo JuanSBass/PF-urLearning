cb20dc5118786cbeb8d5f3d235f51d99f5ace489
JuanSBass <juan_santillan20@hotmail.com>
2022-11-10 18:58:47 -0600

    ruta modificada getSubcatego

--- Client/urLearning/src/redux/actions.js
+++ Client/urLearning/src/redux/actions.js
@@ -5,6 +5,9 @@ export const GET_CHILD_CATEGORY = "GET_CHILD_CATEGORY";
 export const GET_CATEGORY = "GET_CATEGORY";
 export const POST_USER = "POST_USER";
 export const GET_DETAIL = "GET_DETAIL";
+export const FILTER_BY_CATEGORY = "FILTER_BY_CATEGORY";
+export const ORDER_BY_ANY = "ORDER_BY_ANY";
+export const GET_CATEGORIES = "GET_CATEGORIES";
 
 export const getCourses = () => {
   try {
@@ -25,7 +28,7 @@ export function postCourse(dataCourse) {
   };
 }
 
-export function getChildCategory(idCategory) {
+export function getChildCategory(categoryId) {
   return async function (dispatch) {
     const json = await axios.get(
       `/category/childCategoriesFrom?categoryId=${categoryId}`
@@ -66,5 +69,32 @@ export function getCategory() {
       type: GET_CATEGORY,
       payload: json.data,
     });
+  };
+}
+
+export function getCategories() {
+  return async function (dispatch) {
+    const response = await axios.get("/category/allCategories");
+    dispatch({
+      type: GET_CATEGORIES,
+      payload: response.data,
+    });
+  };
+}
+//? <--------- Filters ------------>
+
+export function filteredByCategories(category) {
+  return {
+    type: FILTER_BY_ACTIVITY,
+    payload: category,
+  };
+}
+
+//? <--------- Orders -------->
+
+export function orderByAny(payload) {
+  return {
+    type: ORDER_BY_ANY,
+    payload,
   };
 }
