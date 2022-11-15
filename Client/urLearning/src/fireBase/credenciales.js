eddf1f92c0071da021a886dbbfc559d294488bbb
marco giabbani <mtgiabbani@gmail.com>
2022-11-15 12:48:03 -0300

    prueba importar firebase congif desde env, configura git ignore y env

--- Client/urLearning/src/fireBase/credenciales.js
+++ Client/urLearning/src/fireBase/credenciales.js
@@ -5,15 +5,21 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBrDnc3Zr0dBAqnMg8MEpBeoxLhBIZ5h8k",
-  authDomain: "urlearning-1fe9f.firebaseapp.com",
-  projectId: "urlearning-1fe9f",
-  storageBucket: "urlearning-1fe9f.appspot.com",
-  messagingSenderId: "600786481432",
-  appId: "1:600786481432:web:463817be9f4f2ee4bd2e6d",
-  measurementId: "G-N3V5D1X8K1",
+  apiKey: import.meta.env.VITE_APP_FIREBASE_CONFIG,
+
+  authDomain: "urlearning-e9009.firebaseapp.com",
+
+  projectId: "urlearning-e9009",
+
+  storageBucket: "urlearning-e9009.appspot.com",
+
+  messagingSenderId: "903360110022",
+
+  appId: "1:903360110022:web:76d00bb3ce5489565866c7",
 };
+
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 export const auth = getAuth(firebaseApp);
