941142da19ed44dd743df719a7f0896298eccf08
LucasCanaparo <lucascanaparo@gmail.com>
2022-11-15 22:47:45 -0300

    carrito agrega item y precio beta 2

--- Client/urLearning/src/components/nav/Nav.jsx
+++ Client/urLearning/src/components/nav/Nav.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../img/Logo.png";
 import { Link } from "react-router-dom";
+import ShoppingCart from "../Shopping/ShoppingCart";
 
 
 export default function Nav() {
@@ -61,7 +62,11 @@ export default function Nav() {
 								</a>
 							</li>
 						</Link>
-						<li></li>
+						<li>
+						<a class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent  text-xl md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+								<ShoppingCart></ShoppingCart>
+						 </a>
+						</li>
 					</ul>
 				</div>
 			</div>
--- Client/urLearning/src/redux/cartReducer.js
+++ Client/urLearning/src/redux/cartReducer.js
@@ -2,7 +2,14 @@ import { TYPES } from "./cartActions";
 import { GET_COURSES } from "./actions";
 export const initialState = {
   cart: [],
-  products: [],
+  products: [
+    { id: 1, title: "Curso 1", price: 100 },
+    //{ id: 2, title: "Curso 2", price: 200 },
+    // {id:3,title:"Curso 3",price:300},
+    // {id:4,title:"Curso 4",price:400},
+    // {id:5,title:"Curso 5",price:500},
+    // {id:6,title:"Curso 6",price:600},
+  ],
   courses: [],
   course: {},
   copyCourses: [],
@@ -21,11 +28,29 @@ function cartReducer(state = initialState, action) {
         copyCourses: action.payload,
         coursesForRating: action.payload,
       };
-    case TYPES.ADD_TO_CART:
-      return {
-        ...state,
-        cart: action.payload,
-      };
+    case TYPES.ADD_TO_CART: {
+      //buscar el id en products y guardar en una variable
+      let newItem = state.products.find(
+        (product) => product.id === action.payload
+      ); //cuando el id coincida almacenas el obj
+      console.log(newItem);
+
+      //reviar si el id de prosucto existe -> si es asi hago una modificacion
+      let itemInCart = state.cart.find((item) => item.id === newItem.id); //aqui se guardan los id de los products que esten SOLO en el carrito
+
+      //si el item no esta en el cart le genera el quantity
+      //pero si esta en el cart le suma 1 a esa propiedad
+      return itemInCart
+        ? {
+            ...state,
+            cart: state.cart.map((item) =>
+              item.id === newItem.id
+                ? { ...item, quantity: item.quantity + 1 }
+                : item
+            ),
+          }
+        : { ...state, cart: [...state.cart, { ...newItem, quantity: 1 }] }; //quantity: 1 -> siginifica que es el primer item de ese producto
+    }
 
     case TYPES.REMOVE_ONE_FROM_CART:
       return {
