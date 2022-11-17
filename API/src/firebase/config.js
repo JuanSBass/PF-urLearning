9045dfe7fab35b19de3486aeb6b99646793a3886
JuanSBass <juan_santillan20@hotmail.com>
2022-11-17 10:02:24 -0600

    add .env

--- API/src/firebase/config.js
+++ API/src/firebase/config.js
@@ -1,5 +1,4 @@
 const admin = require("firebase-admin");
-const { PRIVATE_KEY_ID_VALUE, PRIVATE_KEY_VALUE } = process.env;
 const serviceAccount = require("./serviceAccount.json");
 
 admin.initializeApp({
