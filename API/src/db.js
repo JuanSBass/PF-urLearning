cd472043dd5b8098e74ce8ad818933c6b5721e1c
LucasCanaparo <lucascanaparo@gmail.com>
2022-11-17 22:24:14 -0300

    al nivel de lourdes

--- API/src/db.js
+++ API/src/db.js
@@ -59,7 +59,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { User, Course, Category, SubCategory, Cart, Ordes } = sequelize.models;
+const { User, Course, Category, SubCategory, Cart, Order } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
