6d6681f23b475c40c51cc54c4e360fc8eda19c80
marco giabbani <mtgiabbani@gmail.com>
2022-11-17 22:23:29 -0300

    pull origin Santiago

--- API/index.js
+++ API/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, async () => {
+  server.listen(3010, async () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
