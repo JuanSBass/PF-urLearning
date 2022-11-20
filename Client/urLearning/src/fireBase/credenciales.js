456f2f169bb935df030007410c2d0e70142383d3
lupilp <lupilp97@gmail.com>
2022-11-20 19:30:47 -0300

    arreglo firebase

--- Client/urLearning/src/fireBase/credenciales.js
+++ Client/urLearning/src/fireBase/credenciales.js
@@ -7,14 +7,14 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCcRgsrYa3kZSVtbAoK-0TUP9uCSDPd_1Q",
-    authDomain: "urlearning-9e225.firebaseapp.com",
-    projectId: "urlearning-9e225",
-    storageBucket: "urlearning-9e225.appspot.com",
-    messagingSenderId: "766483503136",
-    appId: "1:766483503136:web:fe6823e955337623c0c445",
-    measurementId: "G-N5KY4SBTFN"
-  };
+  apiKey: "AIzaSyAgkHWcbq9beTgvoPc6fhtHKlh-VETM5rc",
+  authDomain: "urlearning-12677.firebaseapp.com",
+  projectId: "urlearning-12677",
+  storageBucket: "urlearning-12677.appspot.com",
+  messagingSenderId: "642638542513",
+  appId: "1:642638542513:web:a41376f064787af8b5d490",
+  measurementId: "G-RNER90V8DY",
+};
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
