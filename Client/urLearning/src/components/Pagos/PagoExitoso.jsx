5906c1455b76f1fce0f8df61d1c1addd03768694
JuanSBass <juan_santillan20@hotmail.com>
2022-11-20 16:22:16 -0600

    agregando titulo al card

--- Client/urLearning/src/components/Pagos/PagoExitoso.jsx
+++ Client/urLearning/src/components/Pagos/PagoExitoso.jsx
@@ -20,37 +20,6 @@ function PagoExitoso() {
 
     }, [dispatch, user, tokken]);
 
-
-    // const products = [
-    //     {
-    //         id: 1,
-    //         name: "Curso de Marco",
-    //         quantity: 1,
-    //         price: 5000,
-    //         image: "https://picaro.news/wp-content/uploads/2022/04/mate.jpg"
-    //     },
-    //     {
-    //         id: 2,
-    //         name: "Aprende guitarra",
-    //         quantity: 1,
-    //         price: 4000,
-    //         image: "https://www.superprof.com.ar/blog/wp-content/uploads/2020/03/aprender-guitarra-principiante-1060x704.jpg"
-    //     },
-    //     {
-    //         id: 3,
-    //         name: "Filosofando entre mates",
-    //         quantity: 1,
-    //         price: 5000,
-    //         image: "https://images.ecestaticos.com/eJpF9C7qs7nMMSPadj5u1uQeukQ=/131x0:863x548/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Ffa6%2Ff7f%2Fe35%2Ffa6f7fe357cc232cf1771a4ce7bc5fb0.jpg"
-    //     },
-    //     {
-    //         id: 4,
-    //         name: "La teoria del dulce de leche",
-    //         quantity: 1,
-    //         price: 2000,
-    //         image: "https://www.finedininglovers.com/es/sites/g/files/xknfdk1706/files/styles/recipes_1200_800_fallback/public/2022-02/dulce-de-leche%C2%A9iStock.jpg?itok=dNtkcbXP"
-    //     },
-    // ];
     return (
         <div className={style.contenedorGeneral}>
             <div className={style.contenedorCosas}>
@@ -60,7 +29,7 @@ function PagoExitoso() {
                         return (
                             <div className={style.contItems}>
                                 <img src={p.image} alt="img" className={style.images} />
-                                {p.name}
+                                {p.title}
                             </div>
 
                         )
