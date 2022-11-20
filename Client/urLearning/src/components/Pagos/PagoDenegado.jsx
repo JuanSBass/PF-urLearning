7b2a1044d1e76af629a681240189837909372cfd
lupilp <lupilp97@gmail.com>
2022-11-20 17:33:52 -0300

    pago denegado

--- Client/urLearning/src/components/Pagos/PagoDenegado.jsx
+++ Client/urLearning/src/components/Pagos/PagoDenegado.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import { Button } from "flowbite-react"
-import style from "../Pagos/PagoExitoso.module.css"
+import style from "../Pagos/PagoDenegado.module.css"
 
 function PagoDenegado() {
     return (
 
         <div className={style.contenedorGeneral}>
             <div className={style.contenedorCosas}>
-                <div className={style.texto}>¡Tu compra fue exitosa!</div>
+                <div className={style.texto}>No pudimos procesar tu pago :(</div>
 
-                <div className={style.texto2}>Comienza a disfrutar</div>
                 <Button
                     gradientDuoTone="purpleToBlue"
                     className={style.buttonGo} >
-                    Ir a mis cursos
+                    Volver al carrito
                 </Button>
             </div>
         </div>
