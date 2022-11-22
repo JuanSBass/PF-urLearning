946e14ee0451c6f8390dd37818c45f5082711f4d
marco giabbani <mtgiabbani@gmail.com>
2022-11-22 13:00:58 -0300

    cambia de nombre el archivo helpers.js a index.js dentro de la misma carpeta helprers. Notese que solo hay una funcion escrita por mi aca adentro

--- API/src/helpers/index.js
+++ API/src/helpers/index.js
@@ -4,6 +4,7 @@ const decodeInfo = async (token) => {
   const decodeValue = await admin.auth().verifyIdToken(token);
   if (!decodeValue) return new Error("no se pudio");
   const userId = decodeValue.uid;
+  return userId;
 };
 
 module.exports = {
