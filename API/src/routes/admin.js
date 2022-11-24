d3307fbe97fe4568e4e24198ccd96959c79e43d1
marco giabbani <mtgiabbani@gmail.com>
2022-11-24 20:23:44 -0300

    saca la ruta de creacion de cursos de admin

--- API/src/routes/admin.js
+++ API/src/routes/admin.js
@@ -4,73 +4,7 @@ const { User, Course, Cart, Category, SubCategory, Order } = require("../db");
 
 ///////////////////Courses///////////////////
 ///////////////////todos///////////////////
-router.post("/course", async (req, res) => {
-  const {
-    title,
-    image,
-    category,
-    subCategory,
-    duration,
-    description,
-    language,
-    price,
-    level,
-    name_prof,
-    videos,
-  } = req.body;
 
-  //console.log(newCourse);
-  const validTitle = await validateTitle(title);
-  const validDescription = await validateDescription(description);
-  const validPrice = await validatePrice(price);
-  const validLevel = await validateLevel(level);
-  const validNameProf = await validateNameProf(name_prof);
-
-  try {
-    console.log(description.length);
-    console.log(validTitle);
-    console.log(level);
-    //console.log(price.length, "dddddd");
-    if (!validTitle || title === "") {
-      res.status(404).send({ message: "Titulo invalido o inexistente" });
-    } else if (category === "") {
-      res.status(404).send({ message: "Categoria inexistente" });
-    } else if (!validDescription || description === "") {
-      res.status(404).send({ message: "Descripcion invalida o inexistente" });
-    } else if (!validPrice) {
-      res
-        .status(404)
-        .send({ message: "El precio NO debe ser menor a 0 o mayor a 100" });
-    } else if (!validLevel || level === "") {
-      res
-        .status(404)
-        .send({ message: "Nivel de dificultad invalido o inexistente" });
-    } else if (!validNameProf || name_prof === "") {
-      res
-        .status(404)
-        .send({ message: "El campo name_prof es invalido o inexistente" });
-    } else {
-      let newCourse = await Course.create({
-        title,
-        image,
-        category,
-        subCategory,
-        duration: Number(duration),
-        description,
-        language,
-        price,
-        level,
-        name_prof,
-        videos,
-      });
-      //console.log(newCourse);
-      res.status(200).send("Curso creado correctamente");
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(404).send(error + "error del /Post Course");
-  }
-});
 router.get("/allCourses", async (req, res) => {
   try {
     let allCourses = await Course.findAll({});
