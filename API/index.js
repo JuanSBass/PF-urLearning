e574afcd1456d6f164821a4b20040c58bd1299a5
marco giabbani <mtgiabbani@gmail.com>
2022-12-05 12:37:06 -0300

    prueba con PORT

--- API/index.js
+++ API/index.js
@@ -1,8 +1,9 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+const { PORT } = process.env;
 
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, async () => {
+  server.listen(PORT, async () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
