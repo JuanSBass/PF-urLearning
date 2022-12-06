495852e4c12eb6c41c1d9948e75de2459066bfdd
Marco Giabbani <81373232+marcogiabbani@users.noreply.github.com>
2022-12-06 12:19:31 -0300

    Update index.js

--- API/index.js
+++ API/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const PORT = process.env.PORT || 3001;
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, async () => {
     console.log("%s listening at", PORT); // eslint-disable-line no-console
   });
