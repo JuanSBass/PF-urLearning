f9b0356cb77f5c6fe46758e609a216abeb766340
Juan Santillán <106228551+JuanSBass@users.noreply.github.com>
2023-01-13 11:36:28 -0600

    Update index.js

--- API/index.js
+++ API/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const PORT = process.env.PORT || 3001;
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, async () => {
     console.log("%s listening at", PORT); // eslint-disable-line no-console
   });
